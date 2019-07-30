3f770c94f0891b6ea047ac27aeffeeac02be6350
hukai <543148827@qq.com>
2019-07-30 14:48:02 +0800

    在usercontroller中加入一些注释以便于理解代码

--- src/main/java/net/suncaper/springboot/controller/UserController.java
+++ src/main/java/net/suncaper/springboot/controller/UserController.java
@@ -17,7 +17,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/search")
+    @GetMapping("/search")    //跳转至userList页面展示查询用户表t_user中的所有元组
     public String goUserListPage(HttpServletRequest request,
                                  HttpServletResponse response,
                                  Model model) {
@@ -25,42 +25,41 @@ public class UserController {
         return "userList";
     }
 
-    @GetMapping("/add")
+    @GetMapping("/add")       //跳转到注册界面user-add
     public String goUserAddPage(Model model) {
         model.addAttribute("user", new User());
         return "user-add";
     }
 
-    @PostMapping("/add")
+    @PostMapping("/add")     //在注册界面提交表单后将数据写入数据库，然后跳转到登录页面login
     public String saveUser(User user) {
         userService.saveUser(user);
-
         return "login";
     }
 
-    @GetMapping("/edit")
-    public String goUserEditPage(@RequestParam("id") String id, Model model) {
-        User user = userService.findUserByPrimaryKey(id);
-        model.addAttribute("user", user);
-        return "user-add";
+    @GetMapping("/login")   //跳转到登录页面login
+    public String goLoginPage(Model model) {
+        model.addAttribute("user", new User());
+        return "login";
     }
 
-
-    @GetMapping("/index")
+    @PostMapping("/login")   //在登录页面提交表单后判断用户名、密码是否正确
+    public String goIndexPage(HttpServletRequest request, String userName, String password, User user, Model model) {
+        User loginUser=userService.login(user);
+        if (loginUser!=null) {
+            request.getSession().setAttribute("USER_ID", "loginUser.getId()");
+        }
+        return  "redirect:/user/index";
+    }
+    @GetMapping("/index")    //主页
     public String goIndexPage(HttpServletRequest request, Model model) {
         model.addAttribute("user", new User());
-
         Boolean isLogin = request.getSession().getAttribute("USER_ID") != null;
         model.addAttribute("isLogin", isLogin);
         return "index";
     }
 
-    @GetMapping("/login")
-    public String goLoginPage(Model model) {
-        model.addAttribute("user", new User());
-        return "login";
-    }
-    @GetMapping("/shoppingcart")
+    @GetMapping("/shoppingcart")  //跳转到购物车页面shoppingcart
     public String goShoppingcartPage(HttpServletRequest request,Model model) {
         model.addAttribute("user", new User());
         Boolean isLogin = request.getSession().getAttribute("USER_ID") != null;
@@ -68,28 +67,33 @@ public class UserController {
         return "/shoppingcart";
     }
 
-
-
-    @GetMapping("/product_0001")
+    @GetMapping("/product_0001")  //跳转至1号商品页面
     public String a(Model model) {
         model.addAttribute("user", new User());
         return "/product_0001";
     }
-    @PostMapping("/login")
 
-    public String goIndexPage(HttpServletRequest request, String userName, String password, User user, Model model) {
-        User loginUser=userService.login(user);
-        if (loginUser!=null) {
-            request.getSession().setAttribute("USER_ID", "loginUser.getId()");
-
-        }
-        return  "redirect:/user/index";
-    }
-
-    @GetMapping("/delete")
+    @GetMapping("/delete")   //delete页面，暂未定义
     @ResponseBody
-    public Boolean deleteUser(@RequestParam("id") String id) {
+    public Boolean deleteUser(@RequestParam("id") String id) {  //在search页面删除一个用户
         userService.deleteUserById(id);
         return true;
     }
+
+
+
+    @GetMapping("/edit")    //目前打开出现错误，与id这个元素不存在有关
+    public String goUserEditPage(@RequestParam("id") String id, Model model) {
+        User user = userService.findUserByPrimaryKey(id);
+        model.addAttribute("user", user);
+        return "user-add";
+    }
+
+    @GetMapping("/select")   //select页面，暂未定义
+    @ResponseBody
+    public User selectUser(@RequestParam("id") String id) {  //在search页面查找一个用户
+        User selectu=userService.findUserByPrimaryKey(id);
+        return selectu;
+    }
+
 }
--- src/main/java/net/suncaper/springboot/service/UserService.java
+++ src/main/java/net/suncaper/springboot/service/UserService.java
@@ -17,43 +17,42 @@ public class UserService {
     @Autowired
     private UserMapper userMapper;
 
-    public void add() {
+    public void add() {     //示例代码，这里没有用到
         SearchUserRequest request = new SearchUserRequest();
         request.setKeyword("%李%");
         request.setAge(10);
         List<User> results = userMapper.search(null, 20);
         System.out.println(results);
-
     }
-    public List<User> listUser() {
+
+    public List<User> listUser() {           //用户管理：调用userMapper里的selectByExample接口方法返回所有t_user中的元组
         return userMapper.selectByExample(new UserExample());
     }
-    public int saveUser(User user) {
+
+    public int saveUser(User user) {        //注册：将表单中的user对象插入数据库t_user表
         if (user.getId() == null|| user.getId().equals("")) {
             return userMapper.insert(user);
         } else {
             return userMapper.updateByPrimaryKeySelective(user);
         }
-
     }
-    public User login(User user) {
+
+    public User login(User user) {       //登录：判断输入的用户名密码是否正确
        User user1= userMapper.selectByName(user.getName());
         if (user1!=null && user1.getPassword().equals(user.getPassword())){
-
             return user1;
         } else {
             return null;
         }
-
     }
-
-
 
     public User findUserByPrimaryKey(String id) {
         return userMapper.selectByPrimaryKey(id);
     }
-    public void deleteUserById(String id) {
+
+    public void deleteUserById(String id) {   //删除用户
         userMapper.deleteByPrimaryKey(id);
     }
+
 }
 
